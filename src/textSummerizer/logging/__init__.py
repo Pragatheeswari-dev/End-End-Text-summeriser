5f0ce497f83c0cfcf7b79b49a5e776cb96ac3761
Pragatheeswari-dev <65264601+Pragatheeswari-dev@users.noreply.github.com>
2024-03-18 12:18:45 +0000

    Utils and  Exceptions are updated

---
+++ src/textSummerizer/logging/__init__.py
@@ -0,0 +1,27 @@
+# custom logging
+
+import os
+import sys
+import logging
+
+# timestamp, log level, file  from where running and message
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+log_dir = "logs"
+log_filepath = os.path.join(log_dir,"running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
+
+# Handlers for logging file  and console/terminal output
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= logging_str,
+
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("textSummarizerLogger")
+
